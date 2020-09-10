cb5968d1e8a0a84e8d026642496f85078cb3fd64
MonteCristo <mjblee20@gmail.com>
2020-09-10 14:18:01 -0700

    crud complete

--- api/todo-api.js
+++ api/todo-api.js
@@ -11,48 +11,45 @@ router.route('/').get((req, res) => {
 });
 
 router.route('/').post((req, res) => {
-  res.send('post');
-  // const task = req.body.task;
+  const task = req.body.task;
 
-  // const todoItem = new Todo({
-  //   task: task,
-  // });
+  const todoItem = new Todo({
+    task: task,
+  });
 
-  // todoItem
-  //   .save()
-  //   .then(() => {
-  //     res.json({
-  //       message: 'New to-do task successfully added',
-  //     });
-  //   })
-  //   .catch((err) =>
-  //     res.status(400).json({
-  //       error: err,
-  //       message: 'Error creating todo task',
-  //     })
-  //   );
+  todoItem
+    .save()
+    .then(() => {
+      res.json({
+        message: 'New to-do task successfully added',
+      });
+    })
+    .catch((err) =>
+      res.status(400).json({
+        error: err,
+        message: 'Error creating todo task',
+      })
+    );
 });
 
 router.route('/update/:id').put((req, res) => {
-  res.send('update');
-  // Todo.findByIdAndUpdate(req.params.id, (err) => {
-  //   if (!err) {
-  //     res.json({ msg: 'successfully updated' });
-  //   } else {
-  //     console.log(err);
-  //   }
-  // });
+  Todo.findByIdAndUpdate(req.params.id, (err) => {
+    if (!err) {
+      res.json({ msg: 'successfully updated' });
+    } else {
+      console.log(err);
+    }
+  });
 });
 
 router.route('/delete/:id').delete((req, res) => {
-  res.send('delete');
-  // Todo.findByIdAndDelete(req.params.id, (err) => {
-  //   if (!err) {
-  //     res.json({ msg: 'successfully deleted' });
-  //   } else {
-  //     console.log(err);
-  //   }
-  // });
+  Todo.findByIdAndDelete(req.params.id, (err) => {
+    if (!err) {
+      res.json({ msg: 'successfully deleted' });
+    } else {
+      console.log(err);
+    }
+  });
 });
 
 module.exports = router;
